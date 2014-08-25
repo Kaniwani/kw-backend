983ce02322d2d5a837ef9159df7a14f3fc73a08d
Gary Graham <garygrantgraham@gmail.com>
2014-08-25 10:59:57 -0400

    Closed #30
    Should still probably show some response indicating that the vocab is
    loading as response time takes a second or two.

--- kw_webapp/static/js/unlocklevels.js
+++ kw_webapp/static/js/unlocklevels.js
@@ -5,8 +5,13 @@ $(document).ready(function() {
 
     $(".locked").click(function(){
         var requested_level = $(this).attr("id");
+        var list_item = $(this)
         console.log("woo");
         $.post("/kw/levelunlock/", {level: requested_level, csrfmiddlewaretoken:csrf_token}).done(function(data) {
+            list_item.removeClass("list-group-item-warning");
+            list_item.removeClass("locked");
+            list_item.addClass("list-group-item-success");
+            list_item.html("Level " + requested_level + " Unlocked")
             alert(data);
         })
     });
