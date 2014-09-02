78b23915088005fc82e22c9088fb6fe4222923b4
Gary Graham <garygrantgraham@gmail.com>
2014-09-02 11:47:11 -0400

    Closed #40 added bug report page
    Added bug report

--- kw_webapp/static/js/unlocklevels.js
+++ kw_webapp/static/js/unlocklevels.js
@@ -2,20 +2,29 @@ $(document).ready(function() {
     var csrf_token = $("#csrf").val();
     //Setting the highlighted tab in the nav bar.
     $("#unlocks_tab").addClass("active");
-    $.notify.defaults({position:"right", autoHideDelay:2000});
+    $(".image").hide();
+    $.notify.defaults({
+        position:"right",
+        autoHideDelay:5000
+        });
+
+    //When you click on an unlockable(yellow) level
     $(".unlockable").click(function(event){
         event.preventDefault();
         var requested_level = $(this).attr("id");
         var list_item = $(this);
+        list_item.find("img").show();
         $.post("/kw/levelunlock/", {level: requested_level, csrfmiddlewaretoken:csrf_token}).done(function(data) {
             list_item.removeClass("list-group-item-warning");
-            list_item.removeClass("locked");
+            list_item.removeClass("unlockable");
+            list_item.addClass("unlocked");
             list_item.addClass("list-group-item-success");
-            list_item.html("Level " + requested_level + " Unlocked");
+            list_item.find("img").hide();
+            list_item.find("p").text("Level " + requested_level + " Unlocked");
             list_item.notify(
-                data + " vocabulary unlocked.",
+                data,
                 {
-                    className:"success",
+                    className:"success"
                 }
             )
         })
@@ -27,7 +36,7 @@ $(document).ready(function() {
         list_item.notify(
                 "I'm Sorry, I can't let you do that. ",
                 {
-                    className:"danger",
+                    className:"danger"
                 }
             )
     });
@@ -38,7 +47,7 @@ $(document).ready(function() {
         list_item.notify(
                 "Already Unlocked!",
                 {
-                    className:"info",
+                    className:"info"
                 }
             )
     });
