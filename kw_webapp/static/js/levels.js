cc61cce41715036da4f0f4b8d5bb84f97f6e04d9
DJTB <duncan.bay@gmail.com>
2015-11-28 16:15:14 +1100

    unlock/lock css classes toggle fix

--- kw_webapp/static/js/levels.js
+++ kw_webapp/static/js/levels.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
     $(document).on("click", ".level__item--locked", function(event) {
         event.preventDefault();
         var item = $(this);
-        var child_icon_span = item.find(".fa-lock");
+        var child_icon_span = item.find(".fa-unlock-alt");
         var level = item.data("level-id");
+        console.log(Date.now());
         $.post("/kw/levelunlock/", {"level": level, csrfmiddlewaretoken:csrf_token}).done(function(data) {
-            console.log(data);
-            child_icon_span.removeClass("fa-lock");
+            console.log(Date.now());
+            console.log(child_icon_span);
+            child_icon_span.removeClass("fa-unlock-alt");
             child_icon_span.addClass("fa-unlock");
-            item.removeClass(".level__item--locked");
+            item.removeClass("level__item--locked");
         });
         event.stopPropagation();
     });
--- kw_webapp/static/js/levelvocab.js
+++ kw_webapp/static/js/levelvocab.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     var csrf_token = $("#csrf").val();
-    $(".fa-unlock").click(function(event) {
+    $('body').click(".fa-unlock", function(event) {
         event.preventDefault();
-        var review_pk = $(this).data("pk");
-        var item = $(this);
+        var $this = $(event.target);
+        console.log($this);
+        var review_pk = $this.data("pk");
+        var item = $this;
          $.post("/kw/togglevocab/", {"review_id": review_pk, csrfmiddlewaretoken:csrf_token}).done(function(data) {
              console.log(data);
              item.removeClass("fa-unlock");
@@ -11,10 +13,11 @@ $(document).ready(function() {
         });
     });
 
-    $(".fa-lock").click(function(event) {
+    $("body").click(".fa-lock", function(event) {
         event.preventDefault();
-        var review_pk = $(this).data("pk");
-        var item = $(this);
+        var $this = $(event.target);
+        var review_pk = $this.data("pk");
+        var item = $this;
         $.post("/kw/togglevocab/", {"review_id": review_pk, csrfmiddlewaretoken:csrf_token}).done(function(data) {
             console.log(data);
             item.removeClass("fa-lock");
