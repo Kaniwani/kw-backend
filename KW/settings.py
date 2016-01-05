55759432df1438256acff87a0ee2f1a665f3daf5
Gary Graham <garygrantgraham@gmail.com>
2016-01-05 09:26:56 -0500

    adds Gzip middleware

--- KW/settings.py
+++ KW/settings.py
@@ -197,6 +197,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = {
