5933c6e02ec545c8842948c6f576d4f4220c4c79
Gary Graham <garygrantgraham@gmail.com>
2016-03-02 15:03:46 -0500

    changing urlpatterns to reflect updates in 1.8

--- KW/settings.py
+++ KW/settings.py
@@ -165,8 +165,6 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.kaniwani.com', '.kaniwani.com']
 
 # Application definition
@@ -291,7 +289,8 @@ TEMPLATES = [
                 "KW.preprocessors.review_count_preprocessor",
                 "KW.preprocessors.srs_count_preprocessor",
                 'django.template.context_processors.request'
-            ]
+            ],
+            "debug": True
         }
     }
 ]
