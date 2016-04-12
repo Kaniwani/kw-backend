1601ea5fb4490a41efb0b57594d32178b6c99806
DJTB <duncan.bay@gmail.com>
2016-04-12 19:44:09 +1000

    remove/comment out prev syncing overlay - prepare /home/ for animate logo and message var testing

--- _front-end/src/assets/toolkit/scripts/sections/home.js
+++ _front-end/src/assets/toolkit/scripts/sections/home.js
@@ -31,18 +31,16 @@ function init() {
 	if (window.location.pathname === '/kw/') {
 		let $refreshButton = $("#forceSrs");
 		let $reviewButton = $("#reviewCount");
-		// TODO: make util functions that get & merge KW from storage with given object
-		// something like (although we still need to sanitize KW.settings and KW.nextReview ... hmm..)
-		// const mergeWithStorageKW = obj => {
-		// 	let KW = Object.assign(simpleStorage.get('KW') || {}, obj);
-		// 	simpleStorage.set('KW', KW);
-		// 	return KW; // so we can do local scope let KW = mergeWithStorageKW(window.KW);
-		// }
-		//
-		// TODO: store on KW object?
-		let recentlySynced = simpleStorage.get('recentlySynced');
 
+/*
+    let recentlySynced = simpleStorage.get('recentlySynced');
 		if (recentlySynced !== true) syncUser();
+*/
+
+    if (true /* !KW.messages.length */) {
+      animateSync();
+    }
+
 		if (!KW.settings.onVacation) {
 			updateReviewTime($reviewButton);
 			KW.reviewTimer = setInterval(() => updateReviewTime($reviewButton), 20000 /* 20s */);
@@ -69,7 +67,7 @@ function updateReviewTime($el) {
 		$el.text(`Next review: ${$.timeago(KW.nextReview)}`);
 	}
 }
-
+/*
 function syncUser() {
 	animateSync();
 
@@ -86,69 +84,30 @@ function syncUser() {
 			notie.alert(3, `Something went wrong while trying to sync with Wanikani. If the problem persists, send us a <a href="/contact/">contact message</a>! with the following: <q class="failresponse">${res.status}: ${res.statusText}</q>`, 10);
 		})
 		.always(() => animateSync({clear: true}));
-}
+}*/
 
-// shortcut to section based on R/S/U/H/C
-function handleKeyPress(key) {
-	let k = key.which;
-	switch(true) {
-		case (k == 82 || k == 114): // R
- 		  window.location.href = "/kw/review/";
-			break;
-		case (k == 83 || k == 115): // S
-			refreshReviews();
-			break;
-		case (k == 85 || k == 117): // U
-			window.location.href = "/kw/unlocks/";
-			break;
-		case (k == 72 || k == 104): // H
-			window.location.href = "/kw/about/";
-			break;
-		case (k == 67 || k == 99): // C
-			window.location.href = "/contact/";
-			break;
-	}
-}
 
-function animateSync({clear = false} = {}) {
-	let $site = $('.site'),
-			$loader = $('.sync-loader');
-
-	if (clear) {
-		$site.removeClass('-syncing');
-		$loader.removeClass('-syncing');
-		return;
-	} else {
-		$site.addClass('-syncing');
-		$loader.addClass('-syncing');
-	}
-
-	const container = $loader.find('.title').get(0);
+function animateSync() {
+	const $logo = document.querySelector('.site-logo');
+  $logo.classList.add('-is-animating')
 	const [blue, purple, pink, tan] = [
 		'hsl(217, 63%, 57%)',
 		'hsl(282, 100%, 47%)',
 		'hsl(314, 100%, 50%)',
 		'hsl(37, 67%, 65%)'
 	];
-	const palette = [ blue, purple, pink, tan]
+	const palette = [ blue, purple, pink, tan ]
 	let paletteIndex = 0;
 
-	setInterval( function() {
-
-	  // Debounce change to allow for css changes
-	  setTimeout( function() {
-	    container.style.color = palette[paletteIndex];
-	    paletteIndex += 1;
-	    paletteIndex %= palette.length;
-	  }, 10 );
-
-	}, 1600 );
+  setInterval( function() {
+    $logo.style.color = palette[paletteIndex];
+    paletteIndex += 1;
+    paletteIndex %= palette.length;
+	}, 1100 );
 }
 
 const api = {
 	init,
-	animateSync,
-	syncUser,
 }
 
 export default api;
