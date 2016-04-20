a8bcc1f2e2ed748c02ac4907221fd7abf43d0f26
Gary Graham <garygrantgraham@gmail.com>
2016-04-20 01:12:13 -0400

    temporary change for testing messages

--- kw_webapp/tasks.py
+++ kw_webapp/tasks.py
@@ -240,6 +240,10 @@ def sync_with_wk(user, full_sync=False):
         #Async messaging system.
         if new_review_count or new_synonym_count:
             message_user(user, "Your Wanikani Profile has been synced. You have {} new reviews, and {} new synonyms".format(new_review_count, new_synonym_count))
+            print("YES WE HAVE SUCCESSFULLY FOUND YOUR NEW VOCAB/WHATEVER")
+        else:
+            message_user(user, "Nothing new to report from Wanikani!")
+            
 
 
         return profile_sync_succeeded, new_review_count, new_synonym_count
