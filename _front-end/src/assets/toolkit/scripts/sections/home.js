f9fc8948785200e7086dd6b6fae1f836a5b434b7
DJTB <duncan.bay@gmail.com>
2016-05-23 12:46:56 +1000

    remove major shortcut keys that navigate between pages

--- _front-end/src/assets/toolkit/scripts/sections/home.js
+++ _front-end/src/assets/toolkit/scripts/sections/home.js
@@ -90,28 +90,6 @@ function syncUser() {
 		.always(() => animateSync({clear: true}));
 }
 
-// shortcut to section based on R/S/U/H/C
-function handleKeyPress(key) {
-	let k = key.which;
-	switch(true) {
-		case (k == 82 || k == 114): // R
- 		  window.location.href = "/kw/review/";
-			break;
-		case (k == 83 || k == 115): // S
-			refreshReviews();
-			break;
-		case (k == 85 || k == 117): // U
-			window.location.href = "/kw/unlocks/";
-			break;
-		case (k == 72 || k == 104): // H
-			window.location.href = "/kw/about/";
-			break;
-		case (k == 67 || k == 99): // C
-			window.location.href = "/contact/";
-			break;
-	}
-}
-
 function animateSync({clear = false} = {}) {
 	let $site = $('.site'),
 			$loader = $('.sync-loader');
