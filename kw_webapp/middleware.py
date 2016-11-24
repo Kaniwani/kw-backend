299ed1104034cd583428c17ff137513030afa6ae
Gary Graham <garygrantgraham@gmail.com>
2016-11-23 21:46:29 -0500

    adds edge case to setting last_visit

--- kw_webapp/middleware.py
+++ kw_webapp/middleware.py
@@ -3,7 +3,6 @@ from kw_webapp.models import Profile
 from kw_webapp.tasks import past_time
 
 
-
 class SetLastVisitMiddleware:
     """
     A middleware class which will update a last_visit field in the profile once an hour.
@@ -11,7 +10,9 @@ class SetLastVisitMiddleware:
     buffer_hours = 1
 
     def process_response(self, request, response):
-        if request.user.is_authenticated() and request.user.profile.last_visit <= past_time(self.buffer_hours):
-            print("UPDATING USER LAST VISIT TO " + str(now()))
+        if request.user.is_authenticated() and self.should_update(request.user):
             Profile.objects.filter(pk=request.user.profile.pk).update(last_visit=now())
         return response
+
+    def should_update(self, user):
+        return user.profile.last_visit is None or user.profile.last_visit <= past_time(self.buffer_hours)
