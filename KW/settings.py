e5f26bf8b9cd811e4c5623dbc220421c54979c89
Gary Graham <garygrantgraham@gmail.com>
2017-01-20 16:39:10 -0500

    adds djangorestframework-jwt, swaps default authentication to using JWTs.

--- KW/settings.py
+++ KW/settings.py
@@ -232,10 +232,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
-
-
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
