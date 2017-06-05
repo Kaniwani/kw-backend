0e88a7022657cb6310824bddd60298f7823e2663
Tadgh <garygrantgraham@gmail.com>
2017-06-05 18:56:23 -0400

    changes for 1.10

--- KW/urls.py
+++ KW/urls.py
@@ -1,20 +1,22 @@
+import django
 from contact_form.views import ContactFormView
 from django.conf.urls import include, url
 
 from django.contrib import admin
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_complete, password_reset_confirm
 from django.views.generic import TemplateView
 from rest_framework import routers
 
 import kw_webapp
 from KW import settings
 from kw_webapp.forms import UserLoginForm, PasswordResetFormCustom, UserContactCustomForm
-from kw_webapp.views import Logout
+from kw_webapp.views import Logout, home
 from kw_webapp.views import Register
 
 admin.autodiscover()
 
 urlpatterns = (
-    url(r'^$', 'kw_webapp.views.home', name='home'),
+    url(r'^$', home, name='home'),
 
     ##API
     #url(r'^api/v1/', include('api.urls', namespace='api')),
@@ -23,23 +25,23 @@ urlpatterns = (
    
 
     ##All Auth Stuff
-    url(r'^auth/login/$', 'django.contrib.auth.views.login',
+    url(r'^auth/login/$', django.contrib.auth.views.login,
         {'template_name': 'registration/login.html',
          'authentication_form': UserLoginForm},
         name="login"),
     url(r'^auth/register/$', Register.as_view(), name="register"),
     url(r'^auth/logout/$', Logout.as_view(), name="logout"),
-    url(r'^auth/password_reset/$', 'django.contrib.auth.views.password_reset',
+    url(r'^auth/password_reset/$', password_reset,
         name="password_reset",
         kwargs={"template_name": "registration/password_reset_form.html",
                 "password_reset_form": PasswordResetFormCustom}),
 
-    url(r'^auth/password_reset/sent/$', 'django.contrib.auth.views.password_reset_done',
+    url(r'^auth/password_reset/sent/$', password_reset_done,
         name="password_reset_done",
         kwargs={"template_name": "registration/password_reset_done.html"}),
     url(r'^auth/password_reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
-    url(r'^auth/password_reset/complete/$', 'django.contrib.auth.views.password_reset_complete',
+        password_reset_confirm, name='password_reset_confirm'),
+    url(r'^auth/password_reset/complete/$', password_reset_complete,
         name="password_reset_complete"),
     url(r'^admin/', include(admin.site.urls)),
 
