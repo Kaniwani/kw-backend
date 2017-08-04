8894f55982a700f557513e8abbdaa67a9b2911a1
Tadgh <garygrantgraham@gmail.com>
2017-08-04 12:51:36 +0200

    hotfix/reestablish autosync upon registration

--- api/signals.py
+++ api/signals.py
@@ -1,10 +1,19 @@
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from djoser.signals import user_registered
 from rest_framework.authtoken.models import Token
+from kw_webapp.tasks import sync_with_wk
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+def sync_unlocks_with_wk(sender, **kwargs):
+    if kwargs['user']:
+        user = kwargs['user']
+        sync_with_wk(user.id, full_sync=user.profile.follow_me)
+
+user_registered.connect(sync_unlocks_with_wk)
