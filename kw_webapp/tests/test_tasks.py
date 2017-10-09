36cf30ba581c301b5049efcad63a55b16bfe4b6a
tadgh <tadgh@cs.toronto.edu>
2017-10-09 12:55:19 -0400

    Adds new user to test merger with

--- kw_webapp/tests/test_tasks.py
+++ kw_webapp/tests/test_tasks.py
@@ -113,7 +113,8 @@ class TestTasks(TestCase):
                       status=200,
                       content_type='application/json')
 
-        checked_levels, unlocked_now_count, total_unlocked_count,  locked_count = unlock_all_possible_levels_for_user(self.user)
+        checked_levels, unlocked_now_count, total_unlocked_count, locked_count = unlock_all_possible_levels_for_user(
+            self.user)
 
         self.assertListEqual(level_list, checked_levels)
         self.assertEqual(total_unlocked_count, 1)
@@ -139,7 +140,7 @@ class TestTasks(TestCase):
         an_hour_ago = now - timezone.timedelta(hours=1)
         two_hours_ago = now - timezone.timedelta(hours=2)
         two_hours_from_now = now + timezone.timedelta(hours=2)
-        four_hours_from_now = now + timezone.timedelta(hours = 4)
+        four_hours_from_now = now + timezone.timedelta(hours=4)
         self.user.profile.vacation_date = two_hours_ago
         self.user.profile.save()
         self.review.last_studied = two_hours_ago
@@ -223,12 +224,11 @@ class TestTasks(TestCase):
     def test_when_reading_level_changes_on_wanikani_we_catch_that_change_and_comply(self):
         resp_body = sample_api_responses.single_vocab_response
 
-        #Mock response so that the level changes on our default vocab.
+        # Mock response so that the level changes on our default vocab.
         responses.add(responses.GET, build_API_sync_string_for_user(self.user),
                       json=sample_api_responses.single_vocab_response,
                       status=200,
                       content_type='application/json')
-
 
         sync_unlocked_vocab_with_wk(self.user)
 
@@ -236,14 +236,12 @@ class TestTasks(TestCase):
 
         self.assertEqual(vocabulary.readings.count(), 1)
 
-
-
     @responses.activate
     def test_when_wanikani_changes_meaning_no_duplicate_is_created(self):
         resp_body = deepcopy(sample_api_responses.single_vocab_response)
         resp_body['requested_information'][0]['meaning'] = "NOT radioactive bat"
 
-        #Mock response so that the level changes on our default vocab.
+        # Mock response so that the level changes on our default vocab.
         responses.add(responses.GET, build_API_sync_string_for_user(self.user),
                       json=resp_body,
                       status=200,
@@ -251,7 +249,7 @@ class TestTasks(TestCase):
 
         sync_unlocked_vocab_with_wk(self.user)
 
-        #Will fail if 2 vocab exist with same kanji.
+        # Will fail if 2 vocab exist with same kanji.
         vocabulary = get_vocab_by_kanji("猫")
 
     @responses.activate
@@ -261,7 +259,7 @@ class TestTasks(TestCase):
         reading2 = create_reading(original_vocabulary, "ねこ", "工作", 2)
         reading3 = create_reading(original_vocabulary, "ねこ", "建設", 2)
         original_review = create_userspecific(original_vocabulary, self.user)
-        assert(len(original_vocabulary.readings.all()) == 3)
+        assert (len(original_vocabulary.readings.all()) == 3)
         original_vocabulary.refresh_from_db()
 
         # Now for kanji 工作, add handicraft to meanings, so that it should be split as meanings no longer identical
@@ -277,38 +275,36 @@ class TestTasks(TestCase):
         original_vocabulary.refresh_from_db()
 
         # We should have stripped the 工作 kanji from the vocabulary.
-        assert(len(original_vocabulary.readings.all()) == 2)
+        assert (len(original_vocabulary.readings.all()) == 2)
 
         # That kanji should now be in it's own vocabulary, which has duplicated the SRS information of the
         # pre-split review.
         new_vocabulary = UserSpecific.objects.filter(user=self.user, vocabulary__meaning="construction, handicraft")
-        assert(len(new_vocabulary.readings.all()) == 1)
-
+        assert (len(new_vocabulary.readings.all()) == 1)
 
         new_review = UserSpecific.objects.get(user=self.user, vocabulary=new_vocabulary)
 
-        #Should have duplicated all KW srs information.
-        assert(new_review.streak == original_review.streak)
-        assert(new_review.correct == original_review.correct)
-        assert(new_review.incorrect == original_review.incorrect)
-        assert(new_review.notes == original_review.notes)
-        assert(new_review.last_studied == original_review.last_studied)
-        assert(new_review.needs_review == original_review.needs_review)
-        assert(new_review.critical == original_review.critical)
-        assert(new_review.hidden == original_review.hidden)
+        # Should have duplicated all KW srs information.
+        assert (new_review.streak == original_review.streak)
+        assert (new_review.correct == original_review.correct)
+        assert (new_review.incorrect == original_review.incorrect)
+        assert (new_review.notes == original_review.notes)
+        assert (new_review.last_studied == original_review.last_studied)
+        assert (new_review.needs_review == original_review.needs_review)
+        assert (new_review.critical == original_review.critical)
+        assert (new_review.hidden == original_review.hidden)
 
         # Any WK information is not duplicated, and is instead read from the API response.
-        assert(new_review.wanikani_burned is True)
-        assert(new_review.wanikani_srs_numeric == 9)
-        assert(new_review.wanikani_srs == "burned")
+        assert (new_review.wanikani_burned is True)
+        assert (new_review.wanikani_srs_numeric == 9)
+        assert (new_review.wanikani_srs == "burned")
 
         # NOTE: Think about Wanikani_burned attribute on UserSpecific. We don't want to duplicate this setting,
         # instead we want to check the API responses during Review clone time.
 
-
     @responses.activate
     def test_if_vocab_meaning_changes_and_it_now_matches_another_vocab_it_is_aglomerated(self):
-        assert(len(self.user.reviews.all()) == 1)
+        assert (len(self.user.reviews.all()) == 1)
         unique_vocabulary = create_vocab("radioactive bat, doggo.")
         create_reading(unique_vocabulary, "いぬ", "犬", 2)
         unique_review = create_userspecific(unique_vocabulary, self.user)
@@ -318,8 +314,8 @@ class TestTasks(TestCase):
         higher_incorrect = unique_review.incorrect = 1
         unique_review.save()
 
-        #Ensure that the user currently has two reviews.
-        assert(len(self.user.reviews.all()) == 2)
+        # Ensure that the user currently has two reviews.
+        assert (len(self.user.reviews.all()) == 2)
 
         # Now for kanji 犬, remove doggo from meanings, leaving only radioactive bat, which should cause a merger.
         # What was originally
@@ -338,18 +334,16 @@ class TestTasks(TestCase):
         sync_unlocked_vocab_with_wk(self.user)
         unique_vocabulary.refresh_from_db()
         found_vocab = get_vocab_by_meaning("radioactive bat")
-        assert(len(found_vocab.readings.all()) == 2)
+        assert (len(found_vocab.readings.all()) == 2)
 
-        #Ensure that the user currently has one review, reflecting the merger. Essentially, make all the same checks
+        # Ensure that the user currently has one review, reflecting the merger. Essentially, make all the same checks
         # that we make in the one time script.
-        assert(len(self.user.reviews.all()) == 1)
+        assert (len(self.user.reviews.all()) == 1)
         only_review = self.user.reviews.first()
-        assert(only_review.correct == higher_correct)
-        assert(only_review.streak == higher_streak)
-        assert(only_review.incorrect == higher_incorrect)
-        assert(only_review.vocabulary.id == found_vocab.id)
-
-
+        assert (only_review.correct == higher_correct)
+        assert (only_review.streak == higher_streak)
+        assert (only_review.incorrect == higher_incorrect)
+        assert (only_review.vocabulary.id == found_vocab.id)
 
     @responses.activate
     def test_one_time_script_for_vocabulary_merging_works(self):
@@ -358,26 +352,32 @@ class TestTasks(TestCase):
         # 2) For each vocabulary, check kanji.
 
         # Option A:
-        # 3) If multiple vocab that have a reading with that kanji are returned, Create *one* new vocab for that kanji, with current info from API.
+
+        # 3) If multiple vocab that have a reading with that kanji are returned, Create *one* new vocab for that kanji,
+        #  with current info from API.
+
         # 3.5) Make sure to copy over the various metadata on the reading we have previously pulled (sentences etc)
 
         # 4) Find all Reviews that point to any of the previous vocabulary objects.
-        # 5) Find maximum of all the reviews when grouped by user. Which has highest SRS, etc. This will be the user's original vocab. Probably best to confirm by checking creation date.
+
+        # 5) Find maximum of all the reviews when grouped by user. Which has highest SRS, etc. This will be the user's
+        # original vocab. Probably best to confirm by checking creation date.
+
         # 6) Point the review's Vocabulary to the newly created vocabulary object from step 3.
-        # 7) Delete all other Vocabulary that are now out of date. This should cascade deletion down to the other reviews.
 
-        # Option B:
-        # 3) If only one vocab is found for a particular kanji, we have successfully *not* created duplicates, meaning the WK vocab has never changed meaning.
-        # 4) We do not have to do anything here. Woohoo!
+        # 7) Delete all other Vocabulary that are now out of date. This should cascade deletion
+        # down to the other reviews.
 
-        #TODO
+        # Option B: 3) If only one vocab is found for a particular kanji, we have successfully *not* created
+        # duplicates, meaning the WK vocab has never changed meaning. 4) We do not have to do anything here. Woohoo!
+
         # Create two vocab, identical kanji, different meanings.
-        v1 = create_vocab("dog") #< -- vestigial vocab.
-        v2 = create_vocab("dog, woofer, pupper") #< -- real, current vocab.
+        v1 = create_vocab("dog")  # < -- vestigial vocab.
+        v2 = create_vocab("dog, woofer, pupper")  # < -- real, current vocab.
         create_reading(v1, "doggo1", "犬", 5)
         create_reading(v2, "doggo2", "犬", 5)
 
-        #Make it so that review 1 has overall better SRS score for the user.
+        # Make it so that review 1 has overall better SRS score for the user.
         review_1 = create_userspecific(v1, self.user)
         review_1.streak = 4
         review_1.correct = 4
@@ -390,11 +390,19 @@ class TestTasks(TestCase):
         review_2.incorrect = 3
         review_2.save()
 
-        #User now has two different vocab, each with their own meaning, however kanji are identical.
+        # Assign another user an old version of the vocab.
+        user2 = create_user("asdf")
+        review_3 = create_userspecific(v1, user2)
+        review_3.streak = 5
+        review_3.correct = 5
+        review_3.incorrect = 0
+        review_3.save()
+
+        # User now has two different vocab, each with their own meaning, however kanji are identical.
 
         # Pull fake "current" vocab. this response, wherein we fetch the data from WK, and it turns out we already
-        # have a local vocabulary with an identical meaning (i.e., we have already stored the correct and currently active
-        # vocabulary.
+        # have a local vocabulary with an identical meaning (i.e., we have already stored the correct and
+        # currently active vocabulary.
         responses.add(responses.GET, build_test_api_string_for_merging(),
                       json=sample_api_responses.single_vocab_existing_meaning_and_should_now_merge,
                       status=200,
@@ -416,13 +424,14 @@ class TestTasks(TestCase):
         self.assertEqual(new_review.next_review_date, review_1.next_review_date)
         self.assertEqual(new_review.last_studied, review_1.last_studied)
 
+        second_users_reviews = UserSpecific.objects.filter(user=user2)
+        self.assertEqual(second_users_reviews.count(), 1)
+        user_two_review = second_users_reviews[0]
+        self.assertEqual(user_two_review.streak, 5)
+        self.assertTrue(user_two_review.vocabulary.meaning == "dog, woofer, pupper")
 
 
 
-
-        # Pull from DB based on kanji + meaning. This is the true current kanji.
-        # Find
-        pass
 
 
 
