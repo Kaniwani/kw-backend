2118c2a0ae247c43d828e8c8bf0f802aab119ecb
Tadgh <tadgh@cs.toronto.edu>
2018-03-12 10:04:28 -0400

    Closes #401. re-adds rounding to next_review_date (#403)

--- kw_webapp/models.py
+++ kw_webapp/models.py
@@ -304,6 +304,7 @@ class UserSpecific(models.Model):
             self.next_review_date = None
         else:
             self.next_review_date = timezone.now() + timedelta(hours=constants.SRS_TIMES[self.streak])
+            self._round_next_review_date()
         self.save()
 
     def set_next_review_time_based_on_last_studied(self):
