0e1b81d256533ee1b17f0f1e82e92faac893bb4f
Tadgh <garygrantgraham@gmail.com>
2020-09-04 00:04:32 -0400

    Add concept of manual whitelisted readings

--- kw_webapp/admin.py
+++ kw_webapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from kw_webapp.models import Announcement
+from kw_webapp.models import Announcement, Vocabulary
 
 # Register your models here.
 
 admin.site.register(Announcement)
+admin.site.register(Vocabulary)
